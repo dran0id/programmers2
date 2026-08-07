df43863f15dae9e19498542e384b2dbee451ed11
user <xixjsfsj@gmail.com>
2026-08-07 23:42:46 +0900

    문자열 돌리기

---
+++ src/main/java/StringRotator.java
@@ -0,0 +1,19 @@
+public class StringRotator {
+    public static void main(String[] args) {
+        /**
+         * 문자열 돌리기
+         *
+         * 문자열 str이 주어집니다.
+         * 문자열을 시계방향으로 90도 돌려서
+         * 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+         */
+        String str = "abcde";
+        solution(str);
+    }
+
+    public static void solution(String a) {
+        for (char ch : a.toCharArray()) {
+            System.out.println(ch);
+        }
+    }
+}
